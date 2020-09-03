737e5d1f210a6d1fe23ffd8e01c6717beb829ea3
matheuscabral <matheuscabralsilva95@gmail.com>
2020-09-03 17:31:43 -0300

    first commit

---
+++ src/database/migrations/1599164270905-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1599164270905 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
