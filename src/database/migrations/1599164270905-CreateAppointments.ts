616bfdbaf7228de581b73e833b2928ad902067d0
Matheus Cabral da Silva <matheuscabralsilva95@gmail.com>
2020-09-04 01:45:35 -0300

    typeORM aplicado

--- src/database/migrations/1599164270905-CreateAppointments.ts
+++ src/database/migrations/1599164270905-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1599164270905 implements MigrationInterfa
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
